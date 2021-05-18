d7ca93ae529cd77e10eef98a245e6818dfabf0df
eemij <eemijun@gmail.com>
2021-05-18 14:13:16 -0700

    Test

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import os
 import shutil
 import tkinter as tk
 from tkinter import *
+from tkinter import simpledialog
 
 
 # Lists each folder and file present in the current working directory
+from tkinter.filedialog import askopenfilename
+
+
 def listDirectories():
     listdir = os.listdir(os.getcwd())
     for x in listdir:
@@ -14,7 +18,7 @@ def listDirectories():
 
 def listDir():
     txt_edit.delete('1.0', END)
-    os.chdir("E:\\")
+    os.chdir(answer + ":\\")
     listdir = os.listdir(os.getcwd())
     for x in listdir:
         txt_edit.insert(END, x + '  ' + str(os.path.getsize(x)) + ' bytes \n')
@@ -22,10 +26,26 @@ def listDir():
 
 def sortList():
     txt_edit.delete('1.0', END)
-    os.chdir("E:\\")
+    os.chdir(answer + ":\\")
     dirlist = os.listdir(os.getcwd())
     dirlist.sort(key=lambda f: os.stat(f).st_size, reverse=True)
     for x in dirlist:
+        txt_edit.insert(END, x + '  ' + str(os.path.getsize(x)) + ' bytes \n')
+
+
+def upList():
+    txt_edit.delete('1.0', END)
+    os.chdir(answer + ":\\")
+    listdir = os.listdir(os.getcwd())
+    for x in listdir:
+        txt_edit.insert(END, x + '  ' + str(os.path.getsize(x)) + ' bytes \n')
+
+
+def dwnList():
+    txt_edit.delete('1.0', END)
+    os.chdir(answer + ":\\")
+    listdir = os.listdir(os.getcwd())
+    for x in listdir:
         txt_edit.insert(END, x + '  ' + str(os.path.getsize(x)) + ' bytes \n')
 
 
@@ -34,112 +54,142 @@ print('Welcome to the Python File Viewer\n')
 # Stores every drive connected on PC in a list.
 drives = [chr(x) + ':' for x in range(65, 90) if os.path.exists(chr(x) + ':')]
 
-#Set-up the window
+# for x in range(len(drives)):
+ #   print(str(5 + x) + '. ' + drives[x])
+
+# Setup window
 window = tk.Tk()
 window.title("File Manager")
 window.resizable(width=True, height=True)
 
-window.rowconfigure(0, minsize=500, weight=1)
-window.columnconfigure(1, minsize=500, weight=1)
+w = 800 # width for the Tk root
+h = 650 # height for the Tk root
 
-txt_edit = tk.Text(window)
-fr_buttons = tk.Frame(window)
-scr = Scrollbar(window, orient=VERTICAL, command=txt_edit.yview)
-btn_open = tk.Button(fr_buttons, text="Refresh", command=listDir)
-btn_save = tk.Button(fr_buttons, text="Sort", command=sortList)
+# get screen width and height
+ws = window.winfo_screenwidth()  # width of the screen
+hs = window.winfo_screenheight()  # height of the screen
 
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+# calculate x and y coordinates for the Tk root window
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
 
-fr_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
+# set the dimensions of the screen
+# and where it is placed
+window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-txt_edit.config(yscrollcommand=scr.set)
+answer = "C"  # Set default
+#answer = simpledialog.askstring("Input", "Which drive do you want to enter? \n" + str(drives))
+
+
+window.columnconfigure(1, weight=1)
+window.columnconfigure(3, pad=7)
+window.rowconfigure(3, weight=1)
+window.rowconfigure(5, pad=7)
+
+lbl = Label(window, text="Windows")
+
+lbl.grid(sticky=W, pady=4, padx=5)
+txt_edit = Text(window)
+txt_edit.grid(row=1, column=0, columnspan=2, rowspan=4,padx=5, sticky=E+W+S+N)
+
+ref_but = Button(window, text="Refresh", width=10, command=listDir)
+ref_but.grid(row=1, column=3)
+
+sort_but = Button(window, text="Sort", width=10, command=sortList)
+sort_but.grid(row=2, column=3, pady=4, padx=10)
+
+help_but = Button(window, text="Help")
+help_but.grid(row=5, column=0, padx=5)
+
+ok_but = Button(window, text="OK")
+ok_but.grid(row=5, column=3)
+
+listDir()
 
 tk.mainloop()
 
 
-while True:
-    showDir = True
-    print("1.Open files/folders \n2.Rename \n3.Move and Paste \n4.Copy and Paste \n")
-    result = input("Choose one of the following: ")
-
-    if result == '1':
-        # Home Screen
-        print('\nQuick Access:\n1. Documents\n2. Videos\n3. Pictures\n4. Downloads\n')
-
-        print('Drives: ')
-        for x in range(len(drives)):
-            print(str(5 + x) + '. ' + drives[x])
-
-        while True:
-            inp = input("\nEnter your Choice: ")
-
-            if inp == '1':
-                path = 'C:\\Users\\$USERNAME\\Documents'
-                os.chdir(os.path.expandvars(path))
-                break
-
-            elif inp == '2':
-                path = 'C:\\Users\\$USERNAME\\Videos'
-                os.chdir(os.path.expandvars(path))
-                break
-
-            elif inp == '3':
-                path = 'C:\\Users\\$USERNAME\\Pictures'
-                os.chdir(os.path.expandvars(path))
-                break
-
-            elif inp == '4':
-                path = 'C:\\Users\\$USERNAME\\Downloads'
-                os.chdir(os.path.expandvars(path))
-                break
-
-            elif int(inp) > 4:
-                os.chdir(str(drives[int(inp) - 5]) + '\\')
-                break
-
-            # elif inp in drives:
-            #    os.chdir(inp + '\\')
-            #    break
-
-            else:
-                print('Error\nEnter a correct input / drive name.\n')
-
-        while True:
-
-            if showDir is True:
-                listDirectories()
-
-            print('\n\nType "sort" to sort list by largest file.')
-            print('Type "exit" to exit from file manager.')
-            print('Type "back" to go up one directory.')
-
-            res = input('\nChoose a file/folder: ')
-            print('\n')
-
-            if res in os.listdir(os.getcwd()):
-                if os.path.isfile(res):
-                    os.system('"' + res + '"')
-                else:
-                    os.chdir(res)
-
-            elif res == 'exit':  # Exit command to exit from loop
-                sys.exit(0)
-
-            elif res == 'back':  # Back command to go up one directory
-                os.chdir('..')
-                showDir = True
-
-            elif res == 'sort':
-                dirlist = os.listdir(os.getcwd())
-                dirlist.sort(key=lambda f: os.stat(f).st_size, reverse=True)
-                for x in dirlist:
-                    print(x + '  ' + str(os.path.getsize(x)) + ' bytes')
-                showDir = False
-
-            else:
-                print('No file/folder exist of this name.')
+# while True:
+#     showDir = True
+#     print("1.Open files/folders \n2.Rename \n3.Move and Paste \n4.Copy and Paste \n")
+#     result = input("Choose one of the following: ")
+#
+#     if result == '1':
+#         # Home Screen
+#         print('\nQuick Access:\n1. Documents\n2. Videos\n3. Pictures\n4. Downloads\n')
+#
+#         print('Drives: ')
+#         for x in range(len(drives)):
+#             print(str(5 + x) + '. ' + drives[x])
+#
+#         while True:
+#             inp = input("\nEnter your Choice: ")
+#
+#             if inp == '1':
+#                 path = 'C:\\Users\\$USERNAME\\Documents'
+#                 os.chdir(os.path.expandvars(path))
+#                 break
+#
+#             elif inp == '2':
+#                 path = 'C:\\Users\\$USERNAME\\Videos'
+#                 os.chdir(os.path.expandvars(path))
+#                 break
+#
+#             elif inp == '3':
+#                 path = 'C:\\Users\\$USERNAME\\Pictures'
+#                 os.chdir(os.path.expandvars(path))
+#                 break
+#
+#             elif inp == '4':
+#                 path = 'C:\\Users\\$USERNAME\\Downloads'
+#                 os.chdir(os.path.expandvars(path))
+#                 break
+#
+#             elif int(inp) > 4:
+#                 os.chdir(str(drives[int(inp) - 5]) + '\\')
+#                 break
+#
+#             # elif inp in drives:
+#             #    os.chdir(inp + '\\')
+#             #    break
+#
+#             else:
+#                 print('Error\nEnter a correct input / drive name.\n')
+#
+#         while True:
+#
+#             if showDir is True:
+#                 listDirectories()
+#
+#             print('\n\nType "sort" to sort list by largest file.')
+#             print('Type "exit" to exit from file manager.')
+#             print('Type "back" to go up one directory.')
+#
+#             res = input('\nChoose a file/folder: ')
+#             print('\n')
+#
+#             if res in os.listdir(os.getcwd()):
+#                 if os.path.isfile(res):
+#                     os.system('"' + res + '"')
+#                 else:
+#                     os.chdir(res)
+#
+#             elif res == 'exit':  # Exit command to exit from loop
+#                 sys.exit(0)
+#
+#             elif res == 'back':  # Back command to go up one directory
+#                 os.chdir('..')
+#                 showDir = True
+#
+#             elif res == 'sort':
+#                 dirlist = os.listdir(os.getcwd())
+#                 dirlist.sort(key=lambda f: os.stat(f).st_size, reverse=True)
+#                 for x in dirlist:
+#                     print(x + '  ' + str(os.path.getsize(x)) + ' bytes')
+#                 showDir = False
+#
+#             else:
+#                 print('No file/folder exist of this name.')
 
     # if result == '2':
     #     print("You chose to rename")
