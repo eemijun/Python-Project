4d177a24ec3c9132459e038b8790fd1aca56a5f8
eemij <eemijun@gmail.com>
2021-06-10 11:19:13 -0700

    Test

---
+++ Tets.py
@@ -0,0 +1,33 @@
+# url = "http\\localhost\\browser\\"
+# url = url[:url.rfind('\\')]
+# print(url)
+# url = url[:url.rfind('\\')]
+# print(url)
+#
+#
+# for x in range(61,90):
+#     print(chr(x))
+
+import time
+import psutil
+
+def main():
+    old_value = 0
+
+    while True:
+        new_value = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+
+        if old_value:
+            send_stat(new_value - old_value)
+
+        old_value = new_value
+
+        time.sleep(1)
+
+def convert_to_gbit(value):
+    return value/1024./1024.*8
+
+def send_stat(value):
+    print ("%0.3f" % convert_to_gbit(value))
+
+main()
